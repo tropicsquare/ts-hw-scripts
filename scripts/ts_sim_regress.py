23fab1f8ea92accac1751fe0ac8762a72cb794f4
jzapeca <jan.zapeca@tropicsquare.com>
2022-12-16 11:34:17 +0000

    Merge branch '161-add-sta-flow-support' into 'master'
    Resolve "add sta flow support"
    
    Closes #161
    
    See merge request internal/development-environment/ts-hw-scripts!144

---
+++ scripts/ts_sim_regress.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python3
+# PYTHON_ARGCOMPLETE_OK
+# -*- coding: utf-8 -*-
+
+####################################################################################################
+# Tropic Square Parallel regression script for digital simulations
+#
+# TODO: License
+####################################################################################################
+
+__author__ = "Ondrej Ille"
+__copyright__ = "Tropic Square"
+__license___ = "TODO:"
+__maintainer__ = "Ondrej Ille"
+
+import logging
+import os
+from concurrent.futures import ProcessPoolExecutor, wait
+from copy import deepcopy
+import argcomplete
+
+from internal import *
+from ts_sim_check import sim_check
+from ts_sim_compile import sim_compile
+
+
+def elaborate_regression_test(test, loop_index):
+    """
+    Elaborate single test.
+    """
+    ts_print(f"Starting test: {test['name']}-{loop_index}", color=TsColors.PURPLE, big=True)
+
+    test["seed"] = ts_generate_seed()
+
+    # Call pre-test hooks
+    ts_call_global_hook(TsHooks.PRE_TEST, test["name"], test["seed"], loop_index)
+    ts_call_local_hook(TsHooks.PRE_TEST_SPECIFIC, test, test["name"], test["seed"], loop_index)
+
+    #######################################################################################
+    # Run elaboration
+    #######################################################################################
+    return ts_sim_elaborate(test)
+
+
+def run_regression_test(test, loop_index, elab_dir):
+    """
+    Run single test.
+    """
+    #######################################################################################
+    # Run simulation
+    #######################################################################################
+    ts_call_global_hook(TsHooks.PRE_SIM)
+
+    sim_log_file = ts_sim_run(test, elab_dir)
+
+    # Call post-test hooks
+    ts_call_local_hook(TsHooks.POST_TEST_SPECIFIC, test, test["name"], test["seed"], loop_index)
+    ts_call_global_hook(TsHooks.POST_TEST, test["name"], test["seed"], loop_index)
+
+    return sim_log_file
+
+
+if __name__ == "__main__":
+
+    init_signals_handler()
+
+    # Add script arguments
+    parser = TsArgumentParser(description="Digital regression script")
+    add_ts_common_args(parser)
+    add_cfg_files_arg(parser)
+    add_target_arg(parser)
+    add_ts_sim_regress_args(parser)
+    argcomplete.autocomplete(parser)
+    args = parser.parse_args()
+    TsGlobals.TS_SIM_CFG_PATH = args.sim_cfg
+    ts_configure_logging(args)
+
+    # Load config file, merge with args and check configuration
+    do_sim_config_init(args)
+    do_design_config_init(args)
+
+    # Fill default config values for command line options which are not available
+    # in ts_sim_regress.py
+    fill_default_config_regress_values(ts_get_cfg())
+
+    # Re-compile if "recompile" is set
+    if ts_get_cfg("recompile"):
+        ts_info(TsInfoCode.INFO_CMN_23)
+        ret_val = sim_compile(args)
+        if ret_val != 0:
+            sys.exit(ret_val)
+
+    # Check target
+    check_target(args.target)
+
+    # Load available tests
+    load_tests()
+
+    # Check that no test is given as a special option
+    if not args.test_name:
+        ts_throw_error(TsErrCode.ERR_SLF_15)
+
+    # Find tests to be executed
+    if len(get_tests_to_run(args.test_name)) == 0:
+        ts_throw_error(TsErrCode.ERR_SLF_14, str(args.test_name))
+
+    # Fill regression iterations if not set for each test
+    for test in TsGlobals.TS_TEST_RUN_LIST:
+        test.setdefault("regress_loops", 1)
+
+    # Print tests to be executed
+    ts_info(TsInfoCode.INFO_CMN_13, get_test_list(TsGlobals.TS_TEST_RUN_LIST, get_repeat=True))
+    ts_info(TsInfoCode.GENERIC, "Number of parallel jobs: {ts_get_cfg('regress_jobs')}")
+
+    ts_call_global_hook(TsHooks.PRE_RUN)
+
+    # Clear log directories always. This makes sure that only logs from this regression
+    # are backed up.
+    shutil.rmtree(ts_get_root_rel_path(TsGlobals.TS_ELAB_LOG_DIR_PATH), ignore_errors=True)
+    shutil.rmtree(ts_get_root_rel_path(TsGlobals.TS_SIM_LOG_DIR_PATH), ignore_errors=True)
+
+    # Create sim_logs, elab_logs directories
+    ts_debug("Create 'sim_logs' directory (if it does not exist)")
+    create_sim_sub_dir(TsGlobals.TS_SIM_LOG_DIR_PATH)
+    ts_debug("Create elaboration log directory")
+    create_sim_sub_dir(TsGlobals.TS_ELAB_LOG_DIR_PATH)
+
+    ###############################################################################################
+    # Execute tests
+    ###############################################################################################
+
+    all_sim_log_files = []
+    all_elab_log_files = []
+
+    futures = []
+
+    with ProcessPoolExecutor(ts_get_cfg("regress_jobs")) as executor:
+        for test in TsGlobals.TS_TEST_RUN_LIST:
+            for i in range(test["regress_loops"]):
+                _test = deepcopy(test)
+                # Elaborate each test one by one - blocking
+                elab_log_file, elab_dir = elaborate_regression_test(_test, i)
+                all_elab_log_files.append(elab_log_file)
+                # Enqueue run job in thread pool - non-blocking
+                futures.append(executor.submit(run_regression_test, _test, i, elab_dir))
+        # Wait until all jobs are finished
+        wait(futures)
+
+    all_sim_log_files = [future.result() for future in futures]
+
+    ts_call_global_hook(TsHooks.POST_RUN)
+
+    ###############################################################################################
+    # Check results.
+    ###############################################################################################
+
+    ret_val = 0
+    if not ts_get_cfg("no_check"):
+
+        # Check elab log files
+        if ts_get_cfg("check_elab_log"):
+            all_sim_log_files.extend(all_elab_log_files)
+
+        # Check sim log files
+        ts_info(TsInfoCode.GENERIC, "Checking log files:")
+        ret_val = sim_check(args, all_sim_log_files)
+
+        ts_call_global_hook(TsHooks.POST_CHECK)
+
+    ###############################################################################################
+    # Backup regression logs
+    ###############################################################################################
+
+    reg_dir = get_regression_dest_dir_name()
+    sim_logs_dir = ts_get_root_rel_path(TsGlobals.TS_SIM_LOG_DIR_PATH)
+    elab_logs_dir = ts_get_root_rel_path(TsGlobals.TS_ELAB_LOG_DIR_PATH)
+
+    ts_info(TsInfoCode.GENERIC, f"Copying regression logs to {reg_dir}")
+
+    os.mkdir(reg_dir)
+    shutil.copytree(sim_logs_dir, os.path.join(reg_dir, os.path.basename(sim_logs_dir)))
+    shutil.copytree(elab_logs_dir, os.path.join(reg_dir, os.path.basename(elab_logs_dir)))
+
+    sys.exit(ret_val)
+
---
+++ scripts/ts_sta_run.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+# PYTHON_ARGCOMPLETE_OK
+# -*- coding: utf-8 -*-
+
+####################################################################################################
+# Tropic Square static timing analysis main script
+#
+# Options
+# scenario (scenario(s) to run)
+# --runcode <runcode>
+#
+# TODO: License
+####################################################################################################
+
+__author__ = "Jan Zapeca"
+__copyright__ = "Tropic Square"
+__license___ = "TODO:"
+__maintainer__ = "Jan Zapeca"
+
+import shutil
+import os
+import argcomplete
+from internal import *
+
+
+if __name__ == "__main__":
+
+    init_signals_handler()
+
+    ################################################################################################
+    # Parse arguments
+    ################################################################################################
+    parser = TsArgumentParser(description="STA script")
+
+    add_ts_common_args(parser)
+    add_cfg_files_arg(parser)
+    add_runcode_arg(parser)
+    add_lic_wait_arg(parser,"pt_shell")
+    add_stayin_arg(parser,"pt_shell")
+    add_ts_sta_run_args(parser,"pt_shell")
+    add_force_arg(parser)
+    add_source_data_arg(parser,"syn")
+    add_release_arg(parser)
+    
+    argcomplete.autocomplete(parser)
+    args = parser.parse_args()
+    ts_configure_logging(args)
+
+    # Prepare file handler
+    sta_logging(args)
+
+    # Do Config Init
+    # Config for ts_design_cfg
+    do_design_config_init(args)
+    # Target need to be defined from TS_DESIGN_CFG (holder of value)
+    setattr(args, "target", TsGlobals.TS_DESIGN_CFG["design"]["target"])
+    # Sim cfg needed for usage of TS_SIM_CFG variable (holder of design name)
+    do_sim_config_init(args)
+    check_valid_design_target()
+    # Execute load of source list according to selected target - ts_hw_souce_list_files
+    load_source_list_files(args.target)
+
+    # Check --source selector valitity
+    check_valid_source_data_arg(args)
+
+    # Check --mode selector validity
+    check_valid_mode_arg(args)
+
+    # Check existance of runcode - runcode is also name of run dir TS_REPO_ROOT/sta/{runcode}
+    if args.runcode is None:
+        ts_throw_error(TsErrCode.ERR_STA_0)
+    else: 
+        ts_info(TsInfoCode.INFO_STA_0,args.runcode)
+        # Set sta flow global variables
+        set_sta_global_vars(args)
+
+    # Check dmsa vs mode selector usage
+    if not (bool(args.dmsa) ^ bool(args.mode)):
+        ts_throw_error(TsErrCode.ERR_STA_6)
+
+    # Check dmsa vs open selector usage
+    if (bool(args.dmsa) and bool(args.open_result)):
+        ts_throw_error(TsErrCode.ERR_STA_7)
+
+    # Check if user requires either to run new synthesis or to open existing synthesis database
+    if args.open_result is False:
+        if args.force is True:
+            ts_info(TsInfoCode.INFO_STA_2,TsGlobals.TS_STA_RUNCODE)
+            # Delete DIR of runcode name if exists
+            delete_sta_sub_dir()
+        else:
+            # Test if database already exists, otherwise create sub-folder
+            if runcode_dir_test(args) is True:
+                ts_throw_error(TsErrCode.ERR_STA_2,TsGlobals.TS_STA_RUNCODE)
+
+        # Create new sub-folder with name of runcode
+        create_sta_sub_dirs()
+
+    else:
+        # Open database
+        if runcode_dir_test(args) is True:
+            ts_info(TsInfoCode.INFO_STA_1,TsGlobals.TS_STA_RUNCODE)
+            sta_open_design(args)
+        # If doesn't exist then error
+        else:
+            ts_throw_error(TsErrCode.ERR_STA_1,TsGlobals.TS_STA_RUNCODE)
+
+
+
+    # Generates design configuration tcl file
+    sta_design_cfg_file(args)
+
+    # Generate synthesis setup tcl file
+    sta_setup(TsGlobals.TS_STA_SETUP_FILE, args)
+
+    # Generate synthesis dmsa setup tcl file
+    if args.dmsa:
+        sta_dmsa_file(TsGlobals.TS_STA_DMSA_FILE, args)
+
+    # Set enviromental variable for DC - license quering
+    set_license_queuing(args,"pt_shell","SNPSLMD_QUEUE")
+
+    # Prepare pt_cmd
+    pt_cmd = build_sta_cmd(args)
+
+    # Run STA
+    exec_cmd_in_dir_interactive(TsGlobals.TS_STA_RUN_DIR,pt_cmd)
+
+    # Goodbye STA!
+    ts_print("STA is done!",color=TsColors.PURPLE,big=True)
+
+    # Release data to a given flow_dir - sta
+    if args.release and TsGlobals.TS_DESIGN_CFG["design"]["flow_dirs"]["sta"] and not args.force:
+        TsGlobals.TS_STA_RELEASE_DIR = join(ts_get_root_rel_path(TsGlobals.TS_DESIGN_CFG["design"]["flow_dirs"]["sta"],TsGlobals.TS_STA_RUNCODE))
+        release(TsGlobals.TS_SYN_RUN_DIR,TsGlobals.TS_SYN_RELEASE_DIR,"sta")
+
+sys.exit(0)
