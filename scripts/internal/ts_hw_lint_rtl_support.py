36de8c811c55b42ecf9faee1f5f8810bc2dcea91
Jan Zapeca <jzapeca@terminal3.tropic.prod.hq.sldev.cz>
2023-04-20 12:11:24 +0200

    added rtl lint

--- scripts/internal/ts_hw_lint_rtl_support.py
+++ scripts/internal/ts_hw_lint_rtl_support.py
@@ -49,9 +49,9 @@ from .ts_hw_logging import (
 )
 
 
-def dft_logging(args):
+def rtl_lint_logging(args):
     """
-    Configures additional handler for logging dft flow file log
+    Configures additional handler for logging flow file log
     :args: Argparse command line arguments object.
     """
 
@@ -60,160 +60,114 @@ def dft_logging(args):
     # Create time stamp for a log file name
     date_time = datetime.now()
     # Set full path and a name of the log file
-    os.makedirs(TsGlobals.TS_DFT_LOGS_DIR, exist_ok=True)
-    filename = f'{TsGlobals.TS_DFT_LOGS_DIR}/{args.runcode}_{date_time.strftime("%y%m%d.%H%M%S")}.log'
+    os.makedirs(TsGlobals.TS_RTL_LINT_LOGS_DIR, exist_ok=True)
+    filename = f'{TsGlobals.TS_RTL_LINT_LOGS_DIR}/{args.runcode}_{date_time.strftime("%y%m%d.%H%M%S")}.log'
     handler = logging.FileHandler(filename)
     handler.setFormatter(TSFormatter(use_colors=False))
     logger.addHandler(handler)
 
 
-def set_dft_global_vars(args):
+def set_rtl_lint_global_vars(args):
     """
-    Set DFT flow global variables
+    Set RTL LINT flow global variables
     """
     # Check runcode validity and update it if the _n+1 rule is applicable
     # Set synthesis run dir according to runcode
-    TsGlobals.TS_DFT_RUNCODE = get_dft_runcode(args, get_dft_rootdir(args))
-    TsGlobals.TS_DFT_RUN_DIR = os.path.join(
-        get_dft_rootdir(args), TsGlobals.TS_DFT_RUNCODE
+    TsGlobals.TS_RTL_LINT_RUNCODE = parse_runcode_arg_root_dir(args, get_rootdir(args))
+    TsGlobals.TS_RTL_LINT_RUN_DIR = os.path.join(
+        get_rootdir(args), TsGlobals.TS_RTL_LINT_RUNCODE
     )
 
     # Set sythesis dirs for purpose of the run
-    TsGlobals.TS_DFT_LOGS_DIR = os.path.join(
-        TsGlobals.TS_DFT_RUN_DIR, TsGlobals.TS_DFT_LOGS_DIR
+    TsGlobals.TS_RTL_LINT_LOGS_DIR = os.path.join(
+        TsGlobals.TS_RTL_LINT_RUN_DIR, TsGlobals.TS_RTL_LINT_LOGS_DIR
     )
-    TsGlobals.TS_DFT_RESULTS_DIR = os.path.join(
-        TsGlobals.TS_DFT_RUN_DIR, TsGlobals.TS_DFT_RESULTS_DIR
+    TsGlobals.TS_RTL_LINT_RESULTS_DIR = os.path.join(
+        TsGlobals.TS_RTL_LINT_RUN_DIR, TsGlobals.TS_RTL_LINT_RESULTS_DIR
     )
-    TsGlobals.TS_DFT_REPORTS_DIR = os.path.join(
-        TsGlobals.TS_DFT_RUN_DIR, TsGlobals.TS_DFT_REPORTS_DIR
+    TsGlobals.TS_RTL_LINT_REPORTS_DIR = os.path.join(
+        TsGlobals.TS_RTL_LINT_RUN_DIR, TsGlobals.TS_RTL_LINT_REPORTS_DIR
     )
-    # Setting path for running DFT
-    TsGlobals.TS_DFT_RUNFILE = os.path.join(
-        TsGlobals.TS_DFT_RUN_DIR, TsGlobals.TS_DFT_RUNFILE
+    # Setting path for running LINT
+    TsGlobals.TS_RTL_LINT_RUNFILE = os.path.join(
+        TsGlobals.TS_RTL_LINT_RUN_DIR, TsGlobals.TS_RTL_LINT_RUNFILE
     )
-    # Setting synthesis build dir for rtl sub-blocks compilation
-    TsGlobals.TS_DFT_BUILD_DIR = os.path.join(TsGlobals.TS_DFT_RUN_DIR, "build")
+    # Setting build dir for rtl sub-blocks compilation
+    TsGlobals.TS_RTL_LINT_BUILD_DIR = os.path.join(
+        TsGlobals.TS_RTL_LINT_RUN_DIR, "build"
+    )
 
     # Setting design_cfg file path and name
-    TsGlobals.TS_DFT_DESIGN_CFG_FILE = os.path.join(
-        TsGlobals.TS_DFT_RUN_DIR, TsGlobals.TS_DFT_DESIGN_CFG_FILE
+    TsGlobals.TS_RTL_LINT_DESIGN_CFG_FILE = os.path.join(
+        TsGlobals.TS_RTL_LINT_RUN_DIR, TsGlobals.TS_RTL_LINT_DESIGN_CFG_FILE
     )
 
     # Setting source file path and name
-    TsGlobals.TS_DFT_SRC_RTL_FILE = os.path.join(
-        TsGlobals.TS_DFT_RUN_DIR, TsGlobals.TS_DFT_SRC_RTL_FILE
+    TsGlobals.TS_RTL_LINT_SRC_RTL_FILE = os.path.join(
+        TsGlobals.TS_RTL_LINT_RUN_DIR, TsGlobals.TS_RTL_LINT_SRC_RTL_FILE
     )
 
-    # Setting DMSA setup file path and name
-    # TsGlobals.TS_DFT_DMSA_FILE = os.path.join(
-    #    TsGlobals.TS_DFT_RUN_DIR, TsGlobals.TS_DFT_DMSA_FILE
-    # )
-    # Setting the netlist
-    if not args.open_result:
-        TsGlobals.TS_DFT_NETLIST = __dft_netlist_selection(args)
-
-    # DFT setup file path
-    TsGlobals.TS_DFT_SETUP_FILE = os.path.join(
-        TsGlobals.TS_DFT_RUN_DIR, TsGlobals.TS_DFT_SETUP_FILE
+    # LINT setup file path
+    TsGlobals.TS_RTL_LINT_SETUP_FILE = os.path.join(
+        TsGlobals.TS_RTL_LINT_RUN_DIR, TsGlobals.TS_RTL_LINT_SETUP_FILE
     )
 
-    # DFT DMSA file path
-    # TsGlobals.TS_DFT_DMSA_FILE = os.path.join(
-    #    TsGlobals.TS_DFT_RUN_DIR, TsGlobals.TS_DFT_DMSA_FILE
-    # )
 
-
-def get_dft_rootdir(args):
+def get_rootdir(args):
     """
-    Get DFT root dir according to flow type
+    Get LINT root dir according to flow type
     """
     # Structure according to agreement with O.Ille
-    if args.flow == "dft_lint":
-        root_dir = f"{get_repo_root_path()}/lint/{TsGlobals.TS_DFT_LINT_TOOL}"
-    elif args.flow == "dft_atpg":
-        root_dir = f"{get_repo_root_path()}/dft/{TsGlobals.TS_DFT_ATPG_TOOL}"
-    elif args.flow == "dft_rtl":
-        root_dir = f"{get_repo_root_path()}/dft/{TsGlobals.TS_DFT_RTL_TOOL}"
-    else:
-        ts_error(TsErrCode.ERR_DFT_3)
+    root_dir = f"{get_repo_root_path()}/lint/{TsGlobals.TS_RTL_LINT_TOOL}"
     return root_dir
 
 
-def get_dft_runcode(args, root_dir):
+def create_rtl_lint_subdirs():
     """
-    Get DFT runcode
-    """
-    return parse_runcode_arg_root_dir(args, root_dir)
-
-
-def __dft_netlist_selection(args):
-    """
-    Search a netlist according to default settings from --source <flow_dir>
-    or try to identify a netlist --netlist <file> [absolute or relative path]
-    """
-    if args.netlist:
-        # Here we use customer entered netlist
-        path = f"{ts_get_root_rel_path(args.netlist)}"
-    elif args.source_data in TsGlobals.TS_DESIGN_CFG["design"]["flow_dirs"]:
-        # Use default netlist location from flow_dirs/results/<design_name>.v
-        path = f'{ts_get_root_rel_path(TsGlobals.TS_DESIGN_CFG["design"]["flow_dirs"][args.source_data])}/{TsGlobals.TS_DFT_RUNCODE}/results/{str(ts_get_design_top()).lower()}.v'
-    else:
-        return None
-
-    if os.path.isfile(path):
-        return path
-    else:
-        ts_throw_error(TsErrCode.ERR_DFT_4, path)
-        return None
-
-
-def create_dft_subdirs():
-    """
-    Create directory structure for DFT
+    Create directory structure for LINT
     """
     # Main runcode subdir
-    ts_info(TsInfoCode.INFO_SYS_4, TsGlobals.TS_DFT_RUN_DIR)
-    os.makedirs(TsGlobals.TS_DFT_RUN_DIR, exist_ok=True)
+    ts_info(TsInfoCode.INFO_SYS_4, TsGlobals.TS_RTL_LINT_RUN_DIR)
+    os.makedirs(TsGlobals.TS_RTL_LINT_RUN_DIR, exist_ok=True)
     # Logs subdir
-    ts_info(TsInfoCode.INFO_SYS_4, TsGlobals.TS_DFT_LOGS_DIR)
-    os.makedirs(TsGlobals.TS_DFT_LOGS_DIR, exist_ok=True)
+    ts_info(TsInfoCode.INFO_SYS_4, TsGlobals.TS_RTL_LINT_LOGS_DIR)
+    os.makedirs(TsGlobals.TS_RTL_LINT_LOGS_DIR, exist_ok=True)
     # Results subdir
-    ts_info(TsInfoCode.INFO_SYS_4, TsGlobals.TS_DFT_RESULTS_DIR)
-    os.makedirs(TsGlobals.TS_DFT_RESULTS_DIR, exist_ok=True)
+    ts_info(TsInfoCode.INFO_SYS_4, TsGlobals.TS_RTL_LINT_RESULTS_DIR)
+    os.makedirs(TsGlobals.TS_RTL_LINT_RESULTS_DIR, exist_ok=True)
     # Report subdir
-    ts_info(TsInfoCode.INFO_SYS_4, TsGlobals.TS_DFT_REPORTS_DIR)
-    os.makedirs(TsGlobals.TS_DFT_REPORTS_DIR, exist_ok=True)
+    ts_info(TsInfoCode.INFO_SYS_4, TsGlobals.TS_RTL_LINT_REPORTS_DIR)
+    os.makedirs(TsGlobals.TS_RTL_LINT_REPORTS_DIR, exist_ok=True)
     # Build subdir
-    ts_info(TsInfoCode.INFO_SYS_4, TsGlobals.TS_DFT_BUILD_DIR)
-    os.makedirs(TsGlobals.TS_DFT_BUILD_DIR, exist_ok=True)
+    ts_info(TsInfoCode.INFO_SYS_4, TsGlobals.TS_RTL_LINT_BUILD_DIR)
+    os.makedirs(TsGlobals.TS_RTL_LINT_BUILD_DIR, exist_ok=True)
 
 
-def delete_dft_subdirs():
+def delete_rtl_lint_subdirs():
     """
     Delete directiories in run folder.
 
     """
-    ts_info(TsInfoCode.INFO_SYS_5, TsGlobals.TS_DFT_RUN_DIR)
-    shutil.rmtree(TsGlobals.TS_DFT_RUN_DIR, ignore_errors=True)
+    ts_info(TsInfoCode.INFO_SYS_5, TsGlobals.TS_RTL_LINT_RUN_DIR)
+    shutil.rmtree(TsGlobals.TS_RTL_LINT_RUN_DIR, ignore_errors=True)
 
 
 def lint_src_file():
     """
-    Generates target RTL DFT TCL script(s)
+    Generates target RTL LINT TCL script(s)
     * Executes the ts_sim_compile.load_source_list_files that prepared data for export
     * This assumes that you have RTL target (--exp-tcl-file-spyglass) defined in your *IP/sim/ts_sim_cfg.yml as ts_sim_config.yml
 
     """
     # Currently supported only 'export_spyglass_src_files'
-    path = f"{TsGlobals.TS_DFT_SRC_RTL_FILE}"
-    cmd = f'export_{TsGlobals.TS_DFT_LINT_TOOL}_src_file("{path}")'
+    path = f"{TsGlobals.TS_RTL_LINT_SRC_RTL_FILE}"
+    cmd = f'export_{TsGlobals.TS_RTL_LINT_TOOL}_src_file("{path}")'
     try:
         exec(cmd)
     except KeyError:
         ts_throw_error(
             TsErrCode.GENERIC,
-            f"Not defined ts_hw_dft_support.export_{TsGlobals.TS_DFT_LINT_TOOL}_src_file() for selected LINT tool {TsGlobals.TS_DFT_LINT_TOOL}",
+            f"Not defined ts_hw_dft_support.{cmd} for selected LINT tool {TsGlobals.TS_RTL_LINT_TOOL}",
         )
 
 
@@ -222,13 +176,13 @@ def build_lint_cmd(args):
     """
     Returns tool command for dft lint
     """
-    cmd = f"build_{TsGlobals.TS_DFT_LINT_TOOL}_cmd(args)"
+    cmd = f"build_{TsGlobals.TS_RTL_LINT_TOOL}_cmd(args)"
     try:
         return eval(cmd)
     except KeyError:
         ts_throw_error(
             TsErrCode.GENERIC,
-            f"Not defined ts_hw_dft_support.build_{TsGlobals.TS_DFT_LINT_TOOL}_cmd() for selected LINT tool {TsGlobals.TS_DFT_LINT_TOOL}",
+            f"Not defined ts_hw_dft_support.build_{TsGlobals.TS_RTL_LINT_TOOL}_cmd() for selected LINT tool {TsGlobals.TS_RTL_LINT_TOOL}",
         )
 
 
@@ -240,35 +194,35 @@ def build_spyglass_cmd(args):
     """
     # Runs sg_shell
     sg_cfg_args = f"-64bit "
-    sg_cfg_args += f"-tcl {TsGlobals.TS_DFT_RUNFILE} "
-    sg_cmd = f'TERM=xterm /usr/bin/bash -c "cd {TsGlobals.TS_DFT_RUN_DIR}; sg_shell {sg_cfg_args}" ;'
+    sg_cfg_args += f"-tcl {TsGlobals.TS_RTL_LINT_RUNFILE} "
+    sg_cmd = f'TERM=xterm /usr/bin/bash -c "cd {TsGlobals.TS_RTL_LINT_RUN_DIR}; sg_shell {sg_cfg_args}" ;'
 
     # Runs spyglass Explorer
     if args.gui:
         sg_cfg_args = f"-64bit "
-        sg_cfg_args += f"-project {TsGlobals.TS_DFT_RUNCODE}.prj "
+        sg_cfg_args += f"-project {TsGlobals.TS_RTL_LINT_RUNCODE}.prj "
         sg_cmd += f"spyglass {sg_cfg_args}"
 
     return sg_cmd
 
 
-# Generic DFT RUN FILE tool regardless
-def dft_runfile(args):
+# Generic RTL LINT RUN FILE tool regardless
+def rtl_lint_runfile(args):
     """
     Returns runfile
     """
-    cmd = f"dft_runfile_{TsGlobals.TS_DFT_LINT_TOOL}(args)"
+    cmd = f"rtl_lint_runfile_{TsGlobals.TS_RTL_LINT_TOOL}(args)"
     try:
         return eval(cmd)
     except KeyError:
         ts_throw_error(
             TsErrCode.GENERIC,
-            f"Not defined ts_hw_dft_support.dft_runfile_{TsGlobals.TS_DFT_LINT_TOOL}(args) for selected LINT tool {TsGlobals.TS_DFT_LINT_TOOL}",
+            f"Not defined ts_hw_dft_support.dft_runfile_{TsGlobals.TS_RTL_LINT_TOOL}(args) for selected LINT tool {TsGlobals.TS_RTL_LINT_TOOL}",
         )
 
 
-# LINT SPYGLASS DFT RUNFILE (project file)
-def dft_runfile_spyglass(args):
+# LINT SPYGLASS RTL RUNFILE (project file)
+def rtl_lint_runfile_spyglass(args):
     """
     Generates main dft batch runfile for spyglass (a project file in spyglass terminology)
     * Imports all generated files
@@ -276,11 +230,11 @@ def dft_runfile_spyglass(args):
     * Imports spyglass constraint file
     """
     # File location
-    path = f"{TsGlobals.TS_DFT_RUNFILE}"
+    path = f"{TsGlobals.TS_RTL_LINT_RUNFILE}"
     # File content
     lines = []
     # Initial settings
-    lines.append(f"new_project {TsGlobals.TS_DFT_RUNCODE} -projectwdir . \n")
+    lines.append(f"new_project {TsGlobals.TS_RTL_LINT_RUNCODE} -projectwdir . \n")
     lines.append(f"set_option sort yes\n")
     lines.append(f"set_option enableSV09 yes\n")
     lines.append(f"set_option hdlin_translate_off_skip_text yes\n")
@@ -288,34 +242,49 @@ def dft_runfile_spyglass(args):
     lines.append(f"set_option pragma {{translate}}\n")
     lines.append(f"set_option define {{TS_MBIST}}\n")
     # Waver
-    lines.append(f"waive -rule ErrorAnalyzeBBox\n")
+    # lines.append(f"waive -rule ErrorAnalyzeBBox\n")
     # Sourcing files and libraries
-    lines.append(f"source {TsGlobals.TS_DFT_DESIGN_CFG_FILE}\n")
-    lines.append(f"read_file -type sglib {TsGlobals.TS_DFT_BUILD_DIR}/*.sglib\n")
-    if args.netlist:
-        lines.append(f"read_file -type hdl {TsGlobals.TS_DFT_NETLIST}\n")
-    else:
-        lines.append(f"source {TsGlobals.TS_DFT_SRC_RTL_FILE}\n")
+    lines.append(f"source {TsGlobals.TS_RTL_LINT_DESIGN_CFG_FILE}\n")
+    lines.append(f"read_file -type sglib {TsGlobals.TS_RTL_LINT_BUILD_DIR}/*.sglib\n")
+    lines.append(f"source {TsGlobals.TS_RTL_LINT_SRC_RTL_FILE}\n")
     # Ignore problematic file - to be removed/fixed
     # lines.append(
     #    f"set_option ignorefile /projects/tropic01/work/{os.getenv('USER')}/ts-common-blocks/rtl/sv/tr_maj_edet_reg_vect.sv\n"
     # )
     # Constraints
-    lines.append(f"read_file -type sgdc {TsGlobals.TS_DFT_CONSTRAINT}\n")
+    lines.append(f"read_file -type sgdc {TsGlobals.TS_RTL_LINT_CONSTRAINT}\n")
     # Compile
     lines.append(f"compile_design -force\n")
     lines.append(f"\n")
+
     # Set goal
     lines.append(
-        f"current_goal dft/dft_scan_ready -top {str(ts_get_design_top()).lower()}\n"
+        f"current_goal lint/lint_rtl -top {str(ts_get_design_top()).lower()}\n"
     )
-    # lines.append(f"set_goal_option scan_ready\n")
     # Run goal
     lines.append(f"run_goal\n")
+
+    # Set goal
     lines.append(
-        f"current_goal dft/dft_best_practice -top {str(ts_get_design_top()).lower()}\n"
+        f"current_goal lint/lint_rtl_enhanced -top {str(ts_get_design_top()).lower()}\n"
     )
     lines.append(f"run_goal\n")
+
+    lines.append(
+        f"current_goal lint/lint_turbo_rtl -top {str(ts_get_design_top()).lower()}\n"
+    )
+    lines.append(f"run_goal\n")
+
+    lines.append(
+        f"current_goal lint/lint_functional_rtl -top {str(ts_get_design_top()).lower()}\n"
+    )
+    lines.append(f"run_goal\n")
+
+    lines.append(
+        f"current_goal lint/lint_abstract -top {str(ts_get_design_top()).lower()}\n"
+    )
+    lines.append(f"run_goal\n")
+
     # Save and exit
     lines.append(f"save_project\n")
     lines.append(f"close_project\n")
@@ -326,18 +295,18 @@ def dft_runfile_spyglass(args):
         dft_runfile.writelines(lines)
 
 
-# Generic DFT design file
+# Generic lint design file
 def lint_design_cfg(args):
     """
     Returns cfg file
     """
-    cmd = f"lint_design_cfg_{TsGlobals.TS_DFT_LINT_TOOL}(args)"
+    cmd = f"lint_design_cfg_{TsGlobals.TS_RTL_LINT_TOOL}(args)"
     try:
         return eval(cmd)
     except KeyError:
         ts_throw_error(
             TsErrCode.GENERIC,
-            f"Not defined ts_hw_dft_support.lint_design_cfg_{TsGlobals.TS_DFT_LINT_TOOL}(args) for selected LINT tool {TsGlobals.TS_DFT_LINT_TOOL}",
+            f"Not defined ts_hw_dft_support.lint_design_cfg_{TsGlobals.TS_RTL_LINT_TOOL}(args) for selected LINT tool {TsGlobals.TS_RTL_LINT_TOOL}",
         )
 
 
@@ -346,7 +315,7 @@ def lint_design_cfg_spyglass(args):
     """
     Generates design configuration TCL script
     * Executes selected procedures from ts_design_cfg script
-    * Generated file to be stored in TS_DFT_RUN_DIR directory
+    * Generated file to be stored in TS_RTL_LINT_RUN_DIR directory
     """
 
     setattr(
@@ -377,21 +346,21 @@ def lint_design_cfg_spyglass(args):
     check_export_view_types(args)
 
     # ts_hw_design_config_file used in ts_design_cfg.py
-    export_design_config(TsGlobals.TS_DFT_DESIGN_CFG_FILE, args)
+    export_design_config(TsGlobals.TS_RTL_LINT_DESIGN_CFG_FILE, args)
 
 
-# Generic DFT lint setup file
+# Generic lint setup file
 def lint_setup_file(args):
     """
     Returns setup file
     """
-    cmd = f"lint_setup_file_{TsGlobals.TS_DFT_LINT_TOOL}()"
+    cmd = f"lint_setup_file_{TsGlobals.TS_RTL_LINT_TOOL}()"
     try:
         return eval(cmd)
     except KeyError:
         ts_throw_error(
             TsErrCode.GENERIC,
-            f"Not defined ts_hw_dft_support.lint_setup_file_{TsGlobals.TS_DFT_LINT_TOOL}(args) for selected LINT tool {TsGlobals.TS_DFT_LINT_TOOL}",
+            f"Not defined ts_hw_dft_support.lint_setup_file_{TsGlobals.TS_RTL_LINT_TOOL}(args) for selected LINT tool {TsGlobals.TS_RTL_LINT_TOOL}",
         )
 
 
@@ -419,7 +388,7 @@ def lint_setup_file_spyglass():
                             TsErrCode.ERR_PDK_25,
                             target_obj_name,
                             target_obj_version,
-                            view,
+                            vnt_rtl_run.py - hiew,
                         )
                     else:
                         ts_warning(
@@ -440,18 +409,18 @@ def lint_setup_file_spyglass():
                             mode["corner"],
                         )
                         continue
-                    cmd += f"spyglass_lc -64bit -gateslib {obj['views'][view][mode['corner']]} -wdir {TsGlobals.TS_DFT_BUILD_DIR};"
+                    cmd += f"spyglass_lc -64bit -gateslib {obj['views'][view][mode['corner']]} -wdir {TsGlobals.TS_RTL_LINT_BUILD_DIR};"
 
     return cmd
 
 
-# Generic DFT lint open design
+# Generic lint open design
 def open_design(args):
     """
     Returns open design command to be run
     """
 
-    cmd = f"{args.flow}_{TsGlobals.TS_DFT_LINT_TOOL}_open_design(args)"
+    cmd = f"{args.flow}_{TsGlobals.TS_RTL_LINT_TOOL}_open_design(args)"
 
     try:
         return eval(cmd)
@@ -462,14 +431,16 @@ def open_design(args):
         )
 
 
-# Lint DFT spyglass open design command
-def dft_lint_spyglass_open_design(args):
+# Lint RTL spyglass open design command
+def rtl_lint_spyglass_open_design(args):
 
     # Runs spyglass Explorer
     sg_cfg_args = f"-64bit "
-    sg_cfg_args += f"-project {TsGlobals.TS_DFT_RUNCODE}.prj "
+    sg_cfg_args += f"-project {TsGlobals.TS_RTL_LINT_RUNCODE}.prj "
 
     sg_cmd = f"spyglass {sg_cfg_args}"
-    sg_cmd = f'TERM=xterm /usr/bin/bash -c "cd {TsGlobals.TS_DFT_RUN_DIR}; {sg_cmd}" ;'
+    sg_cmd = (
+        f'TERM=xterm /usr/bin/bash -c "cd {TsGlobals.TS_RTL_LINT_RUN_DIR}; {sg_cmd}" ;'
+    )
 
     return sg_cmd
--- scripts/ts_lint_rtl_run.py
+++ scripts/ts_lint_rtl_run.py
@@ -3,7 +3,7 @@
 # -*- coding: utf-8 -*-
 
 ####################################################################################################
-# Tropic Square ts_dft_run.py dft script
+# Tropic Square ts_lint_rtl_run.py dft script
 #
 # Options
 # scenario (scenario(s) to run)
@@ -26,9 +26,9 @@ from internal.ts_hw_args import (
     TsArgumentParser,
     add_batch_mode_arg,
     add_cfg_files_arg,
-    add_dft_lint_args,
     add_force_arg,
     add_lic_wait_arg,
+    add_lint_rtl_args,
     add_release_arg,
     add_runcode_arg,
     add_source_data_arg,
@@ -46,20 +46,20 @@ from internal.ts_hw_common import (
     init_signals_handler,
     ts_get_root_rel_path,
 )
-from internal.ts_hw_dft_support import (
+from internal.ts_hw_global_vars import TsGlobals
+from internal.ts_hw_lint_rtl_support import (
     build_lint_cmd,
-    create_dft_subdirs,
-    delete_dft_subdirs,
-    dft_logging,
-    dft_runfile,
-    get_dft_rootdir,
+    create_rtl_lint_subdirs,
+    delete_rtl_lint_subdirs,
+    get_rootdir,
     lint_design_cfg,
     lint_setup_file,
     lint_src_file,
     open_design,
-    set_dft_global_vars,
+    rtl_lint_logging,
+    rtl_lint_runfile,
+    set_rtl_lint_global_vars,
 )
-from internal.ts_hw_global_vars import TsGlobals
 from internal.ts_hw_logging import (
     TsColors,
     TsErrCode,
@@ -78,38 +78,18 @@ if __name__ == "__main__":
     ################################################################################################
     # Parse arguments
     ################################################################################################
-    parser = argparse.ArgumentParser(description="Main DFT flow script")
-
-    # Sub-parsers
-    subparsers = parser.add_subparsers(
-        title="Available DFT flow types",
-        description="lint - formal verification in spyglass, atpg - automatic test pattern generatior, rtl - insert dft structures into RTL",
-    )
+    parser = TsArgumentParser(description="Main LINT RTL script")
 
     # LINT parser
-    parser_lint = subparsers.add_parser("lint")
-    parser_lint.set_defaults(flow="dft_lint")
-    parser_lint.set_defaults(filter_mode_usage="dft_lint")
-    add_runcode_arg(parser_lint)
-    add_dft_lint_args(parser_lint, TsGlobals.TS_DFT_LINT_TOOL)
-    add_release_arg(parser_lint)
-    add_batch_mode_arg(parser_lint)
-    add_source_data_arg(parser_lint, "dft_lint")
-    add_stayin_arg(parser_lint, TsGlobals.TS_DFT_LINT_TOOL)
-    add_force_arg(parser_lint)
-    add_cfg_files_arg(parser_lint)
-    add_ts_common_args(parser_lint)
-
-    # ATPG parser
-    parser_atpg = subparsers.add_parser("atpg")
-    parser_atpg.set_defaults(flow="dft_atpg")
-    parser_atpg.set_defaults(filter_mode_usage="dft_atpg")
-    add_stayin_arg(parser_atpg, TsGlobals.TS_DFT_ATPG_TOOL)
-
-    # RTL parser
-    parser_rtl = subparsers.add_parser("rtl", description="DFT RTL insertion flow")
-    parser_rtl.set_defaults(flow="dft_rtl")
-    add_stayin_arg(parser_rtl, TsGlobals.TS_DFT_RTL_TOOL)
+    add_runcode_arg(parser)
+    add_lint_rtl_args(parser, TsGlobals.TS_RTL_LINT_TOOL)
+    add_release_arg(parser)
+    add_batch_mode_arg(parser)
+    add_source_data_arg(parser, "rtl_lint")
+    add_stayin_arg(parser, TsGlobals.TS_RTL_LINT_TOOL)
+    add_force_arg(parser)
+    add_cfg_files_arg(parser)
+    add_ts_common_args(parser)
 
     # Process args
     argcomplete.autocomplete(parser)
@@ -118,7 +98,7 @@ if __name__ == "__main__":
     ts_configure_logging(args)
 
     # Prepare file handler
-    dft_logging(args)
+    rtl_lint_logging(args)
 
     # Do Config Init
     # Config for ts_design_cfg
@@ -126,6 +106,9 @@ if __name__ == "__main__":
 
     # Target need to be defined from TS_DESIGN_CFG (holder of value)
     setattr(args, "target", TsGlobals.TS_DESIGN_CFG["design"]["target"])
+
+    # Set flow_type
+    setattr(args, "flow", "rtl_lint")
 
     # Sim cfg needed for usage of TS_SIM_CFG variable (holder of design name)
     do_sim_config_init(args)
@@ -143,66 +126,66 @@ if __name__ == "__main__":
     else:
         ts_info(TsInfoCode.INFO_DFT_0, args.runcode)
         # Set the flow global variables
-        set_dft_global_vars(args)
+        set_rtl_lint_global_vars(args)
 
     # Check if user requires either to run new lint or to open existing lint database
     if args.open_result is False:
         if args.force is True:
             ts_info(TsInfoCode.INFO_SYS_2, args.runcode)
             # Delete DIR of runcode name if exists
-            delete_dft_subdirs()
+            delete_rtl_lint_subdirs()
         else:
             # Test if database already exists, otherwise create sub-folder
             if (
                 os.path.isdir(
-                    os.path.join(get_dft_rootdir(args), TsGlobals.TS_DFT_RUNCODE)
+                    os.path.join(get_dft_rootdir(args), TsGlobals.TS_RTL_LINT_RUNCODE)
                 )
                 is True
             ):
                 ts_throw_error(TsErrCode.ERR_SYN_2, args.runcode)
 
         # Create new sub-folder with name of runcode
-        create_dft_subdirs()
+        create_rtl_lint_subdirs()
 
     else:
         # Open database
         if (
-            os.path.isdir(os.path.join(get_dft_rootdir(args), TsGlobals.TS_DFT_RUNCODE))
+            os.path.isdir(
+                os.path.join(get_rootdir(args), TsGlobals.TS_RTL_LINT_RUNCODE)
+            )
             is True
         ):
             ts_info(TsInfoCode.INFO_SYS_1, args.runcode)
             open_design_cmd = open_design(args)
-            exec_cmd_in_dir(TsGlobals.TS_DFT_RUN_DIR, open_design_cmd, args.batch_mode)
+            exec_cmd_in_dir(
+                TsGlobals.TS_RTL_LINT_RUN_DIR, open_design_cmd, args.batch_mode
+            )
             sys.exit(0)
         # If doesn't exist then error
         else:
             ts_throw_error(TsErrCode.ERR_SYN_1, args.runcode)
 
-    if args.flow == "dft_lint":
+    # Generates necessary source files
+    lint_src_file()
 
-        # Generates necessary source files
-        lint_src_file()
+    # Generates dft run file
+    rtl_lint_runfile(args)
 
-        # Generates dft run file
-        dft_runfile(args)
+    # Generates design configuration tcl file
+    lint_design_cfg(args)
 
-        # Generates design configuration tcl file
-        lint_design_cfg(args)
+    # Generate lint setup tcl file
+    lint_cmd = lint_setup_file(args)
+    exec_cmd_in_dir(TsGlobals.TS_RTL_LINT_RUN_DIR, lint_cmd, args.batch_mode)
 
-        # Generate lint setup tcl file
-        lint_cmd = lint_setup_file(args)
-        exec_cmd_in_dir(TsGlobals.TS_DFT_RUN_DIR, lint_cmd, args.batch_mode)
+    # Prepare lint_cmd
+    # Uses dft runfile
+    lint_cmd = build_lint_cmd(args)
 
-        # Prepare lint_cmd
-        # Uses dft runfile
-        lint_cmd = build_lint_cmd(args)
+    # Run LINT
+    exec_cmd_in_dir(TsGlobals.TS_RTL_LINT_RUN_DIR, lint_cmd, args.batch_mode)
 
-        # Run LINT
-        exec_cmd_in_dir(TsGlobals.TS_DFT_RUN_DIR, lint_cmd, args.batch_mode)
-
-        # Goodbye message
-        ts_print("DFT is done!", color=TsColors.PURPLE, big=True)
-    else:
-        ts_print("DFT cannot be run!", color=TsColors.PURPLE, big=True)
+    # Goodbye message
+    ts_print("LINT RTL is done!", color=TsColors.PURPLE, big=True)
 
 sys.exit(0)
